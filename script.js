06e9036e334f57e3141588b49aa164ed561f533c
Yujin <yujinpetercho@gmail.com>
2024-04-11 09:55:21 -0500

    added confirmPassword and password match validation

--- script.js
+++ script.js
@@ -2,5 +2,10 @@ document.querySelector("#loginForm-button").addEventListener("click", (e) => {
     if (document.querySelector("input#password").value != 
     document.querySelector('input#confirm_password').value) {
         e.preventDefault();
+        document.querySelector("input#password").style.borderColor = "red";
+        document.querySelector("input#confirm_password").style.borderColor = "red";
+
+        document.querySelector("input#password + span").classList.add("passwordMismatch");
+        document.querySelector("input#confirm_password + span").classList.add("passwordMismatch");
     }
 })
